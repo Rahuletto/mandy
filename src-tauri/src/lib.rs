7dee3aea0f18cbdf099bc0ab63d660a2c52df840
Rahul Marban <rahulmarban@gmail.com>
2026-03-28 11:12:30 +0530

    feat: liquid glass

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -54,6 +54,7 @@ pub fn run() {
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_fs::init())
+        .plugin(tauri_plugin_liquid_glass::init())
         .manage(ws_registry);
 
     #[cfg(target_os = "macos")]
